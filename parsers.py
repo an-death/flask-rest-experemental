c7c7925564314b05e6294865cd5a174ccf68033c
An-Death <saenarayoursorrow@gmail.com>
2018-01-31 01:26:02 +0300

    Add required key to all calculate_parser fields

--- parsers.py
+++ parsers.py
@@ -1,16 +1,17 @@
 from flask_restful import reqparse
 
-
+default_help_string = 'Required field <{}> cannot be empty'
 def get_parsers():
     parsers = {
         'book_parser': reqparse.RequestParser(),
         'calculate_parser': reqparse.RequestParser()
     }
 
-    parsers['calculate_parser'].add_argument('name', type=str, help='name cannot be empty')
-    parsers['calculate_parser'].add_argument('email', type=str, help='email cannot be empty')
-    parsers['calculate_parser'].add_argument('phone', type=str, help='phone cannot be empty')
-    parsers['calculate_parser'].add_argument('books', type=list, help='books cannot be empty')
+    parsers['calculate_parser'].add_argument('name', type=str, required=True, help=default_help_string.format('name'))
+    parsers['calculate_parser'].add_argument('email', type=str, required=True, help=default_help_string.format('email'))
+    parsers['calculate_parser'].add_argument('phone', type=str, required=True, help=default_help_string.format('phone'))
+    parsers['calculate_parser'].add_argument('books', type=list, required=True,
+                                             help=default_help_string.format('books'))
 
     parsers['book_parser'].add_argument('category', type=str)
     parsers['book_parser'].add_argument('cost', type=float)
