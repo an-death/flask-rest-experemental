1a2653c4f8b94875e709f6c87df8d34203c495cf
An-Death <saenarayoursorrow@gmail.com>
2018-01-31 00:02:10 +0300

    Models: Transaction + method create_transaction

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 class Dev:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///./test.db'
-    SQLALCHEMY_BINDS = {'test': 'sqlite:////test.db'}
+    SQLALCHEMY_BINDS = {'test': 'sqlite:///./test.db'}
 
 
 class Ops:
--- models/models.py
+++ models/models.py
@@ -1,3 +1,5 @@
+import hashlib
+from datetime import datetime
 from os import urandom
 
 from sqlalchemy.orm import validates
@@ -9,14 +11,13 @@ from app import db
 class Meta(db.Model):
     __abstract__ = True
     __tablename__ = None
-
     def __repr__(self):
         return ('<{}({})>'.format(self.__tablename__, ', '.join(
             _ + ': ' + str(atr) for _, atr in self.__dict__.items() if not _.startswith('_'))))
 
 
 books_transactions = db.Table('association_books_transactions', Meta.metadata,
-                              db.Column('transaction_id', db.Integer, db.ForeignKey('transaction.id')),
+                              db.Column('transaction_id', db.Integer, db.ForeignKey('transaction.transaction_hash')),
                               db.Column('book_id', db.Integer, db.ForeignKey('books.id'))
                               )
 
@@ -26,10 +27,15 @@ class Customer(Meta):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
-    email = db.Column(db.String(200))
+    email = db.Column(db.String(200), unique=True)
     phone_number = db.Column(db.String)  # todo upgrade validation on sa_utils
 
     transactions = db.relationship('Transaction', backref=db.backref('customer'))
+
+    def __init__(self, name, email, phone_number):
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
 
     @validates('email')
     def validate_email(self, key, address):
@@ -47,12 +53,27 @@ class Customer(Meta):
 class Transaction(Meta):
     __tablename__ = 'transaction'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    transaction_hash = db.Column(db.Text, unique=True)
+    transaction_hash = db.Column(db.Text, unique=True, primary_key=True)
     customer_id = db.Column(db.Integer, db.ForeignKey(Customer.id))
     date = db.Column(db.DateTime)
-    purchase_cost = db.Column(CurrencyType)
+
     books = db.relationship('Books', secondary=books_transactions, back_populates='transactions')
+
+    def __init__(self, transaction_hash, customer_id, books: list):
+        self.transaction_hash = transaction_hash
+        self.customer_id = customer_id
+        self.date = datetime.now()
+        for book in books:
+            assert isinstance(book, Books)
+            self.books.append(book)
+
+    @classmethod
+    def create_transaction(cls, customer_id, books: list):
+        books = [Books.query.filter(Books.ISBN == ISBN).first_or_404() for ISBN in books if ISBN]
+        transaction_hash = hashlib.md5((''.join(str(b.id) for b in books)).encode()).hexdigest()
+
+        trans = cls(transaction_hash, customer_id, books)
+        return trans
 
 
 class Category(Meta):
@@ -62,6 +83,9 @@ class Category(Meta):
     name = db.Column(db.String(200), unique=True)
 
     books = db.relationship('Books', backref=db.backref('category'))
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Books(Meta):
@@ -74,3 +98,25 @@ class Books(Meta):
     description = db.Column(db.Text(300), default=urandom(200))
 
     transactions = db.relationship('Transaction', secondary=books_transactions, back_populates='books')
+
+    def __init__(self, ISBN, category_id, cost, description=None):
+        self.ISBN = ISBN
+        self.category_id = category_id
+        self.cost = cost
+        self.description = description
+
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from database import Dev
+
+    engine = create_engine(Dev.SQLALCHEMY_DATABASE_URI)
+    db.metadata.bind = engine
+    db.drop_all(engine)
+    db.create_all(engine)
+    book = Books(ISBN='01-0101-0111', category_id=1, cost=500)
+    horrors = Category(name='horrors')
+    business = Category(name='business')
+    book2 = Books(ISBN='01-0101-0110', category_id=2, cost=200.2)
+    db.session.add_all([horrors, business, book, book2])
+    db.session.commit()
